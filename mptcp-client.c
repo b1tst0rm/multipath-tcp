1b4937d014f8d9e8e33ccd6e3231b28174f28ec5
Daniel Limanowski <dlimanow@iastate.edu>
2018-11-19 23:42:00 -0600

    create main

---
+++ mptcp-client.c
@@ -0,0 +1,24 @@
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define CONTROL_PORT = 50000
+#define DATA_PORT_1 = 50001
+#define DATA_PORT_2 = 50002
+#define DATA_PORT_3 = 50003
+
+
+int main (int argc, char const *argv[])
+{
+        char base_data[] = "0123456789"
+                           "abcdefghijklmnopqrstuvwxyz"
+                           "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int i = 0;
+
+        for (i = 0; i < 16; i++) {
+                printf("%s\n", base_data);
+        }
+
+        return EXIT_SUCCESS;
+}

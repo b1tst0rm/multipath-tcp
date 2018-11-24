bee16c366c8c485480d9f4f5e7e65f0d390f7c8b
Daniel Limanowski <dlimanow@iastate.edu>
2018-11-23 20:58:38 -0600

    begin adjusting pipes

--- mptcp-client.c
+++ mptcp-client.c
@@ -20,41 +20,39 @@
 #define SERV_IP "127.0.0.1"
 #define ROUND_ROBIN_SIZE 4
 
+
 // FUNCTION PROTOTYPES
 int create_subflow(int port);
-void subflow(int init_pipe[2], int write_pipe[2], int read_pipes[3][2]);
+void subflow(int init_pipe[2], int write_pipes[3][2], int read_pipes[3][2]);
 
 
 int main (int argc, char const *argv[])
 {
-        char base_data[] = "0123456789"
+        char base_data[62] = "0123456789"
                            "abcdefghijklmnopqrstuvwxyz"
                            "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        printf("%d\n", (int) strlen(base_data));
         int ctl_fd, ctl_retval, i;
         char buffer[1024] = {0};
         char message[] = "Test temporary message.";
-        int parent_pipe[2]; // Parent writes, child reads 
-        int child_pipe[2]; // Child writes, parent reads 
-        int private_pipes[3][2]; // Child writes, parent reads
+        int init_pipe[2]; // Parent writes, child reads...only used to administer child IDs
+        int child_pipes[3][2]; // Child writes, parent reads 
+        int parent_pipes[3][2]; // Parent writes, child reads
 
         // Create pipes for process-to-process communication
         for (i = 0; i < 3; i++) {
-                if (pipe(private_pipes[i]) < 0) {
+                if (pipe(child_pipes[i]) < 0 || pipe(parent_pipes[i]) < 0) {
                         perror("private pipe");
                         exit(EXIT_FAILURE);
                 }
         }
 
-        if (pipe(parent_pipe) < 0) {
+        if (pipe(init_pipe) < 0) {
                 perror("parent pipe");
                 exit(EXIT_FAILURE);
         }
 
-        if (pipe(child_pipe) < 0) {
-                perror("child pipe");
-                exit(EXIT_FAILURE);
-        }
-        
         ctl_fd = create_subflow(CTL_PORT);
         int sf_fd1 = create_subflow(DATA_PORT_1);
         int sf_fd2 = create_subflow(DATA_PORT_2);
@@ -64,32 +62,31 @@ int main (int argc, char const *argv[])
         int fork2 = fork();
         if (fork1 == 0 || fork2 == 0) {
                 // child process
-                subflow(parent_pipe, child_pipe, private_pipes);
+                subflow(init_pipe, child_pipes, parent_pipes);
         } else if (fork1 < 0 || fork2 < 0) {
                 perror("fork");
                 exit(EXIT_FAILURE);
         } else {
                 // parent process
                 char resp1, resp2, resp3;
-                char msg1[ROUND_ROBIN_SIZE] = "sto1";
-                char msg2[ROUND_ROBIN_SIZE] = "sto2";
-                char msg3[ROUND_ROBIN_SIZE] = "sto3";
-                close(parent_pipe[0]); // close input to write
-                close(private_pipes[0][0]); // close input to write
-                close(private_pipes[1][0]); // close input to write
-                close(private_pipes[2][0]); // close input to write
-                close(child_pipe[1]); // close output to read
+                close(init_pipe[0]); // close input to write
+                close(child_pipes[0][0]); // close input to write
+                close(child_pipes[1][0]); // close input to write
+                close(child_pipes[2][0]); // close input to write
+                close(parent_pipe[0][1]); // close output to read
+                close(parent_pipe[1][1]); // close output to read
+                close(parent_pipe[2][1]); // close output to read
 
                 unsigned short ids[3];
                 ids[0] = 0;
                 ids[1] = 1;
                 ids[2] = 2;
                 // Write child IDs to shared pipe
-                write(parent_pipe[1], &ids[0], sizeof(unsigned short));
-                write(parent_pipe[1], &ids[1], sizeof(unsigned short));
-                write(parent_pipe[1], &ids[2], sizeof(unsigned short));
+                write(init_pipe[1], &ids[0], sizeof(unsigned short));
+                write(init_pipe[1], &ids[1], sizeof(unsigned short));
+                write(init_pipe[1], &ids[2], sizeof(unsigned short));
 
-                close(parent_pipe[1]); // close pipe as no longer used
+                close(init_pipe[1]); // close pipe as no longer used
 
                 // Write connection FDs to private pipes
                 write(private_pipes[0][1], &sf_fd1, sizeof(int));
@@ -102,10 +99,28 @@ int main (int argc, char const *argv[])
                 read(child_pipe[0], &resp3, sizeof(char));
                 printf("Responses: %c %c %c\n", resp1, resp2, resp3);
 
-                // Write message to the children
-                write(private_pipes[0][1], &msg1, ROUND_ROBIN_SIZE);
-                write(private_pipes[1][1], &msg2, ROUND_ROBIN_SIZE);
-                write(private_pipes[2][1], &msg3, ROUND_ROBIN_SIZE);
+                // Build the entire 992-byte payload
+                char total_msg[992];
+                for (i = 0; i < 16; i++) {
+                        strncat(total_msg, base_data, 62);
+                }
+
+                // Write message to the children round-robin style
+                int child_index = 0;
+                unsigned short num = 0; // represents the DSS (overarching sequence number of transmission
+                for (i = 0; i < 992; i = i + 4) {
+                        strncpy(msg, total_msg[i], ROUND_ROBIN_SIZE);
+                        // Parent writes the sequence number followed by the ROUND_ROBIN_SIZE message
+                        // - the child ACKs by returning the sequence number...if sequence number
+                        // is wrong, then the parent throws an error and exits
+                        write(private_pipes[child_index][1], &num, sizeof(unsigned short));
+                        write(private_pipes[child_index][1], &msg, ROUND_ROBIN_SIZE);
+                        if (child_index == 2) {
+                                child_index = 0;
+                        } else {
+                                child_index++;
+                        }
+                }
 
                 // End the children with "STOP"
                 char stop[ROUND_ROBIN_SIZE] = "STOP";
@@ -174,7 +189,7 @@ int create_subflow(int port)
 /* TODO: Cleanup...thought: have a shared read_pipe and an array of child-process-specific
 read_pipes. The children get their "ID" from the shared pipe and then access 
 their index in the array of read_pipes */
-void subflow(int init_pipe[2], int write_pipe[2], int read_pipes[3][2])
+void subflow(int init_pipe[2], int write_pipes[3][2], int read_pipes[3][2])
 {
 
         unsigned short child_id; /* child gets an ID that tells them what index in 
@@ -193,8 +208,8 @@ void subflow(int init_pipe[2], int write_pipe[2], int read_pipes[3][2])
         
         // Acknowledge receipt of socket fd by writing Y to pipe (Y = yes)
         char yes = 'Y';
-        close(write_pipe[0]);
-        write(write_pipe[1], &yes, sizeof(char));
+        close(write_pipes[child_id][0]);
+        write(write_pipes[child_id][1], &yes, sizeof(char));
 
         // Parent process will send child processes ROUND_ROBIN_SIZE byte chunks to send to
         // the server. Once the entirety of the message has been sent, the
